27cdd785e0907848cacd76714654c683b52c5d8d
kristof komlossy <kristof.komlossy@uni-weimar.de>
2017-06-23 00:31:03 +0200

    +started dataManager

--- app/src/main/java/de/uni_weimar/touchpivot/DataManager.java
+++ app/src/main/java/de/uni_weimar/touchpivot/DataManager.java
@@ -1,7 +1,23 @@
 package de.uni_weimar.touchpivot;
 
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.content.res.Resources;
+
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 /**
@@ -9,7 +25,15 @@ import java.util.ArrayList;
  */
 
 public class DataManager {
+    private Resources resources;
+    private Activity activity = null;
     private ArrayList<JSONObject> list_data_items = new ArrayList<>();
+
+    public DataManager(MainActivity mainActivity) {
+        activity = mainActivity;
+        resources = activity.getResources();
+        init();
+    }
 
     public void add_item(JSONObject obj) {
         list_data_items.add(obj);
@@ -19,6 +43,66 @@ public class DataManager {
     }
     public ArrayList<JSONObject> get_items() {
         return list_data_items;
+    }
+
+    private void init() {
+        this.load_data_items();
+        ListView dataTable  = (ListView)activity.findViewById(R.id.dataTable);
+        DataArrayAdapter adapter = new DataArrayAdapter(activity, get_items());
+        dataTable.setAdapter(adapter);
+    }
+
+
+    private void load_data_items() {
+        try {
+            System.out.println(new InputStreamReader(resources.openRawResource(R.raw.file_short)));
+            BufferedReader br = new BufferedReader( new InputStreamReader(resources.openRawResource(R.raw.file_short)));
+            for(String line; (line = br.readLine()) != null; ) {
+                JSONObject obj = new JSONObject(line);
+                this.add_item(obj);
+                this.add_item(obj);
+                this.add_item(obj);
+                this.add_item(obj);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private class DataArrayAdapter extends ArrayAdapter<JSONObject> {
+
+        public DataArrayAdapter(Context context, ArrayList<JSONObject> objects) {
+            super(context, 0, objects);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            // Get the data item for this position
+            JSONObject dataItem = getItem(position);
+            // Check if an existing view is being reused, otherwise inflate the view
+            if (convertView == null) {
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.data_item, parent, false);
+            }
+            // Lookup view for data population
+            TextView textViewID = (TextView) convertView.findViewById(R.id.textViewID);
+            TextView textViewName = (TextView) convertView.findViewById(R.id.textViewName);
+            TextView textViewCount = (TextView) convertView.findViewById(R.id.textViewCount);
+//            TextView tvHome = (TextView) convertView.findViewById(R.id.tvHome);
+            // Populate the data into the template view using the data object
+            try {
+                textViewID.setText(dataItem.getString("id"));
+                textViewName.setText(dataItem.getString("name"));
+                textViewCount.setText(dataItem.getString("count_of_something"));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            // Return the completed view to render on screen
+            return convertView;
+        }
     }
 }
 
--- app/src/main/java/de/uni_weimar/touchpivot/MainActivity.java
+++ app/src/main/java/de/uni_weimar/touchpivot/MainActivity.java
@@ -1,9 +1,13 @@
 package de.uni_weimar.touchpivot;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TableLayout;
-import android.widget.TableRow;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import org.json.JSONException;
@@ -15,53 +19,18 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
-    private DataManager dataManager = new DataManager();
-    private TableLayout dataTable = null;
+    private DataManager dataManager = null;
+    private ListView dataTable = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        dataTable = (TableLayout)findViewById(R.id.dataTable);
-        fill_table();
-    }
-
-    private void fill_table() {
-        load_data_items();
-        for(JSONObject item: dataManager.get_items()) {
-            TableRow newTableRow = new TableRow(this);
-            TextView textview = new TextView(this);
-            try {
-                textview.setText(item.getString("name"));
-            } catch (JSONException e) {
-                e.printStackTrace();
-                textview.setText("EXCEPTION OCCURRED");
-            }
-            newTableRow.addView(textview);
-            dataTable.addView(newTableRow);
-        }
-    }
-
-    private void load_data_items() {
-        try {
-            FileInputStream fileIn = openFileInput("file_short.ldjson");
-            BufferedReader br = new BufferedReader( new InputStreamReader(fileIn));
-            for(String line; (line = br.readLine()) != null; ) {
-                JSONObject obj = new JSONObject(line);
-                dataManager.add_item(obj);
-                dataManager.add_item(obj);
-                dataManager.add_item(obj);
-                dataManager.add_item(obj);
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+        dataTable = (ListView)findViewById(R.id.dataTable);
+        dataManager = new DataManager(this);
     }
 }
